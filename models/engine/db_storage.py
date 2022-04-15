eb6fb1f99d311cf08da67e5f192769ebe223926d
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2022-04-16 00:39:58 +0300

    Improve storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,13 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Returns object based on the class and its ID, or None if not found"""
+        cls = classes.get(cls, None)
+        return self.__session.query(cls).filter(cls.id == id).first() \
+            if cls else None
+
+    def count(self, cls=None):
+        """Returns the number of objects in storage matching the given class"""
+        return len(self.all(cls))
